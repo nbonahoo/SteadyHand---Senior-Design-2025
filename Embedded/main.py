f7effd6082d297e8f6b1f9b8c6285880c7220264
nbonahoo <142938346+nbonahoo@users.noreply.github.com>
2025-10-09 19:32:19 -0400

    added embedded software

--- Embedded/main.py
+++ Embedded/main.py
@@ -75,10 +75,13 @@ while True:
 # pmA0 = machine.PWM(pA0)
 # pmA1 = machine.PWM(pA1)
 # pm33 = machine.PWM(p33)
-# pmA0.freq(500)
+# pmA0.freq(40)
 # pmA1.freq(1000)
 # pm33.freq(750)
-# pmA0.duty(300)
+# pmA0.duty(512)
+# print("Outputting PWM Wave")
+# while True:
+#     pass
 # pmA1.duty(300)
 # pm33.duty(300)
 # 
